3c4c328f6469441d2cb639da1d7dcbf5dbfde618
Edwin Obando <edwin.alex1995@gmail.com>
2023-01-22 23:36:12 -0500

    v2 - 22/01/2023

--- _next/static/847dCYEywkqjkBt30yx1_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ea4522f8952b3232.css","static/chunks/pages/index-1d7193a9ab3933f2.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
